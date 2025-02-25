b5e0e5283ef01e2ae5ebf9b7e4241807f3ea19d7
Arvind S <183717903+arvisre@users.noreply.github.com>
2025-02-25 15:03:05 +0000

    Delete crazyforloop.sh

--- crazyforloop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#i was wondering what if i want to print each directory path in the PATH variable line by line
-#i used the cut -d: -f but what value will i enter for f  ?
-#so i did echo $PATH and counted 10 directory paths
-#that's when i wanted to write a for loop.
-for var in {1..10}
-do
-echo $PATH | cut -d: -f$var
-done
