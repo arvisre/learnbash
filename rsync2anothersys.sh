57d29d16bf593deeb0678d0f3da7059daf69af20
Arvind S <183717903+arvisre@users.noreply.github.com>
2025-02-25 01:20:32 +0000

    rsync2anothersys.sh

--- rsync2anothersys.sh
+++ rsync2anothersys.sh
@@ -17,3 +17,7 @@ systemctl start ssh
 rsync /home/as/myscripts/universalupdatescript.sh as@192.168.2.20:/home/as/
 
 #transfer file to remote system using rsync
+#The script asked me for a password to start the ssh service in the source system. 
+#Then a prompt to fingerprint. 
+#Then the password for the destination system. 
+#how can I make a case for less human intervention ?
